759d9b21011aae88df5621b1a75d1a36aa8f41f1
sabahat-12345 <sabahatali976@gmail.com>
2025-03-29 15:55:37 +0500

    Add debug print statements for count conversion in DemoDialog

--- lib/custom_dialog.dart
+++ lib/custom_dialog.dart
@@ -85,6 +85,53 @@ class _DemoDialogState extends State<DemoDialog> {
         });
   }
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             children: [
+//               CustomButton(
+//                   ontap: () {
+//                     customDialog(context);
+//                   },
+//                   title: "Dialog",
+//                   backgroundColor: Colors.red),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               CustomButton(
+//                   ontap: () {},
+//                   title: "SnackBar",
+//                   backgroundColor: Colors.green),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//  }
+
+  customSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: Colors.grey.shade400,
+      content: Text(
+        "This item has been removed",
+        style: TextStyle(
+            color: Colors.black, fontSize: 14, fontWeight: FontWeight.w400),
+      ),
+      duration: const Duration(seconds: 2),
+      action: SnackBarAction(
+        label: "undo",
+        onPressed: () {},
+        backgroundColor: Colors.green,
+      ),
+    ));
+  }
+
+  int count = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,10 +139,16 @@ class _DemoDialogState extends State<DemoDialog> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CustomButton(
                   ontap: () {
-                    customDialog(context);
+                    // customDialog(context);
+                    String stringCount = count.toString();
+                    debugPrint("this is the string count $stringCount");
+                    int intCount = int.parse(stringCount);
+                    double doubleCount = double.parse(stringCount);
+                    debugPrint("this is the double count $doubleCount");
                   },
                   title: "Dialog",
                   backgroundColor: Colors.red),
@@ -103,7 +156,9 @@ class _DemoDialogState extends State<DemoDialog> {
                 height: 20,
               ),
               CustomButton(
-                  ontap: () {},
+                  ontap: () {
+                    customSnackBar(context);
+                  },
                   title: "SnackBar",
                   backgroundColor: Colors.green),
             ],
@@ -112,4 +167,4 @@ class _DemoDialogState extends State<DemoDialog> {
       ),
     );
   }
-}
+} 
